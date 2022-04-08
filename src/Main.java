5326f0a4354003c0e012958701f0bd384b0b1942
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-08 13:48:01 -0500

    java arrays practice

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+
 public class Main {
 
 
-    static void FizzBuzz() {
-        for (int i = 1; i <= 50; i++)  {
-            if (i % 15 == 0) System.out.println("FizzBuzz");
-            else if(i % 3 == 0) System.out.println("Fizz");
-            else if(i % 5 == 0) System.out.println("Buzz");
-            else System.out.println(i);
-        }
-    }
-
-    static int Fibonachi(int a) {
-        int y = 0;
-        int z = 1;
-        for(int i = 0; i < a; i++) {
-            int x = y;
-            y = z;
-            z = z + x;
-        }
-        return y;
-    }
-
-    static String ReverseString(String str) {
-        String revString = "";
-        char ch;
-        for (int i = 0; i < str.length(); i++) {
-            ch = str.charAt(i);
-            revString = ch + revString;
-        }
-
-        return revString;
-    }
-
-
-    static boolean isPalindrome(String str) {
-        if (str.equalsIgnoreCase(ReverseString(str))) {
-            return true;
-        }
-        return false;
-    }
-
-    static String stripVowels(String str) {
-        str = str.replaceAll("[aeiou]", "");
-        return str;
+    public static int add(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
-        FizzBuzz();
-        System.out.println(ReverseString("hello"));
-        System.out.println(isPalindrome("dod"));
-        System.out.println(stripVowels("HelloMoto"));
-        for(int i = 0; i <= 10; i++) {
-            System.out.println(Fibonachi(i));
+        System.out.println(add(2, 5));
+        // arrays
+        double[] prices;
+        prices = new double[4];
+        prices[0] = 12.25;
+        prices[1] = 11.12;
+        prices[2] = 23.79;
+        prices[3] = 8.24;
+
+        for(int i = 0; i < prices.length; i++) {
+            System.out.println(prices[i]);
         }
+
     }
 }
