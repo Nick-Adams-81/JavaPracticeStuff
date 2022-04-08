7fd5b7c2cdd3036a08fc89d92e3303bba31cdb3c
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-08 13:54:00 -0500

    added two dimensional array

--- src/Main.java
+++ src/Main.java
@@ -31,5 +31,14 @@ public class Main {
             System.out.println(n);
         }
 
+        // two-dimensional array
+        int [][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        System.out.println(matrix[1][0]);
+        System.out.println(matrix[2][2]);
+
     }
 }
