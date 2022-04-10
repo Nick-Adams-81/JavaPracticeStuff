4b892a1eea36a1862980fb4c439713f82d62bb1a
Nick Adams <nick.pappas.adams@gmail.com>
2022-04-10 18:43:06 -0500

    added fizzbuzz

--- src/Main.java
+++ src/Main.java
@@ -41,7 +41,7 @@ public class Main {
     }
 
     public static void fizzBuzz() {
-        for(int i = 0; i < 50; i++) {
+        for(int i = 1; i <= 50; i++) {
             if(i % 15 == 0) System.out.println("FizzBuzz");
             else if(i % 3 == 0) System.out.println("Fizz");
             else if(i % 5 == 0) System.out.println("Buzz");
