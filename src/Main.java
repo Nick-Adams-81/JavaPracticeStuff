937b14fd177ae15e96a76ddf2f4732a9f80fea0b
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-13 13:43:58 -0500

    added array list

--- src/Main.java
+++ src/Main.java
@@ -93,6 +93,15 @@ public class Main {
         }
 
         // array lists
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(1, 3);
+        numbers.add(2);
+
+        for(int i = 0; i < numbers.size(); i++) {
+            System.out.println(numbers.get(i));
+        }
+
 
     }
 }
